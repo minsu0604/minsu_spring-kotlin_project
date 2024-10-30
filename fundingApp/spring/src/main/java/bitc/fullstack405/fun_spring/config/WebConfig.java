3c0eee452d9b375608b10a08af7f3b2ac0509cb3
minsu <story604604@gmail.com>
2024-10-30 12:34:38 +0900

    no message

--- fundingApp/spring/src/main/java/bitc/fullstack405/fun_spring/config/WebConfig.java
+++ fundingApp/spring/src/main/java/bitc/fullstack405/fun_spring/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // "/image/**" 경로로 요청이 들어오면 "C://FullStack405/react/team1_project/spring/image/"에서 이미지를 찾기
-        registry.addResourceHandler("/image/**")
+        registry.addResourceHandler("image/**")
                 .addResourceLocations("C://FullStack405/react/team1_project/spring/image/");
     }
 }
